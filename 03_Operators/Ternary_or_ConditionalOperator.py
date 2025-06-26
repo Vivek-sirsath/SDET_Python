5864f4f0b6a1357aff41a98f4125ef5e5122cf9c
vivek <vivek.sirsath1@gmail.com>
2025-06-26 07:58:16 +0530

    Variable name changed from 'a' to 'name'

--- 03_Operators/Ternary_or_ConditionalOperator.py
+++ 03_Operators/Ternary_or_ConditionalOperator.py
@@ -3,8 +3,8 @@
 # a if condition TRUE returns a else b
 # a if condition FALSE returns b
 
-a = "Vivek" if 10>5 else "Ishita"
-print(a)  # Vivek
+name = "Vivek" if 10>5 else "Ishita"
+print(name)  # Vivek
 
-a = "Vivek" if 10<5 else "Ishita"
-print(a)  # Ishita
+name = "Vivek" if 10<5 else "Ishita"
+print(name)  # Ishita
