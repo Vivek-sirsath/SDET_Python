898a021f49332adf18545ddd743bc1cf82582879
vivek <vivek.sirsath1@gmail.com>
2025-06-29 23:37:36 +0530

    Range Function added in SliceOperatorOfString.py

--- 03_Operators/SliceOperatorOfString.py
+++ 03_Operators/SliceOperatorOfString.py
@@ -1,11 +1,20 @@
 # Slice Operator of String
 # Reference - 06_String_Part_01
 
+# SLICE OPERATOR IN CASE OF STRING OF NUMBERS :
+# -------------------------------------------
 #   print(s[ 1:9 ])
 #           /   \
 #          /     \
 #  Exclusive     Inclusive
 
+
+# SLICE OPERATOR IN CASE OF LIST OF NUMBERS :
+# -----------------------------------------
+#    print(myList[2:6])
+#                /   \
+#               /     \
+#        Inclusive    Exclusive
 
 s = "123456789"
 
@@ -25,3 +34,13 @@ print("---------------------------------------------")
 print(s[::])  # 123456789
 print(s[4:100000:1])  # 56789
 print(s[100000:5:1])  # Blank_output
+
+print("=============== Range Function ===============")
+
+myList = [00,10,20,30,40,50,60,70,80,90]
+print(f"myList:", myList)   # myList: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+print(myList[2:6])   # [20, 30, 40, 50]
+print(myList[2:8:2])   # [20, 40, 60]
+print(myList[1:9:2])   # [10, 30, 50, 70]
+
