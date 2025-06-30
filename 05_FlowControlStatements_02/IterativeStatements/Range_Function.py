6dec3c1334556c36dd3f43b8499ebf9ef579e6e9
vivek <vivek.sirsath1@gmail.com>
2025-07-01 01:16:05 +0530

    Inclusive and Exclusive parameters of Range Function added in Range_Function.py

--- 05_FlowControlStatements_02/IterativeStatements/Range_Function.py
+++ 05_FlowControlStatements_02/IterativeStatements/Range_Function.py
@@ -5,17 +5,23 @@
 # range(start, stop)  --->  form 2 (Mostly used)
 # range(start, stop, step)  --->  form 3 (Mostly used)
 
+#    print(list(range(5,10)))
+#                    /   \
+#                   /     \
+#           Inclusive     Exclusive
+
 range(10)  # form 1
 print(range(10))  # range(0, 10) # 0 comes by default
 print(list(range(10))) # Converting range dataType to list dataType  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print(list(range(5,10))) # [5, 6, 7, 8, 9]
-print(list(range(5,10,1))) # [5, 6, 7, 8, 9]   # By default it is 1
+print(list(range(5,10))) # [5, 6, 7, 8, 9]   # By default step is 1
+print(list(range(5,10,1))) # [5, 6, 7, 8, 9]   # step of 1
 print(list(range(5,10,2))) # [5, 7, 9]  # step of 2 (difference of 2)
 print(list(range(1,10,3))) # [1, 4, 7]
 
 print(list(range(10,1,-1))) # [10, 9, 8, 7, 6, 5, 4, 3, 2]
 print(list(range(-10,-5))) # [-10, -9, -8, -7, -6]
+print(list(range(-5,1)))  # [-5, -4, -3, -2, -1, 0]
 print(list(range(10,1,1))) # []
 print(list(range(10,15,-1))) # []
 
