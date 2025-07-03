79ccf7ca6bdfe6ae638ed061947d1504d897922a
vivek <vivek.sirsath1@gmail.com>
2025-07-04 00:37:40 +0530

    1_PositionalArgument.py modified

--- 13_FunctionsInPython_Part_02/1_PositionalArgument.py
+++ 13_FunctionsInPython_Part_02/1_PositionalArgument.py
@@ -5,28 +5,9 @@ div(10,20)  # 0.5
 div(20,10)  # 2.0   # Result changed due to changing the position of arguments.
 
 
+# Errors may come if we change the number of arguments.
 
+# div(20)   # TypeError: div() missing 1 required positional argument: 'j'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+div(20,10,30)   # TypeError: div() takes 2 positional arguments but 3 were given
 
