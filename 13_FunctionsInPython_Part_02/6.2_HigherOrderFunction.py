58b08996c8f91a35586bab625f9e52b19ff41029
vivek <vivek.sirsath1@gmail.com>
2025-07-06 02:02:27 +0530

    Example of calculating AreaOfCircle added in 6.2_HigherOrderFunction.py

--- 13_FunctionsInPython_Part_02/6.2_HigherOrderFunction.py
+++ 13_FunctionsInPython_Part_02/6.2_HigherOrderFunction.py
@@ -1,5 +1,7 @@
 # HIGHER ORDER FUNCTION :
 
+# EXAMPLE 1 :-
+
 def apply_function(func,value):
     return func(value)
 def square(num):
@@ -9,14 +11,16 @@ def square(num):
 
 print(apply_function(square,3))  # 9
 
+print("=======================================================================")
 
+# EXAMPLE 2 :-
 
+def areaOfCircle(func,radius):
+    return func(radius)
+def squareOfRadius(radius):
+    return 3.14*radius**2
 
-
-
-
-
-
+print(areaOfCircle(squareOfRadius,5))  # 78.5
 
 
 
