002dbebb6b67f36229729e6de03eae38682a40ea
vivek <vivek.sirsath1@gmail.com>
2025-07-07 00:06:27 +0530

    comment added in WithStatementOrContextManager.py in File Handling directory

--- 15_FileHandling_Part_01/WithStatementOrContextManager.py
+++ 15_FileHandling_Part_01/WithStatementOrContextManager.py
@@ -1,6 +1,7 @@
 # CONTEXT MANAGER or 'WITH' STATEMENT :
 
 # Purpose of the context manager is to close file automatically once perform operation we can go for context manager.
+# Because sometimes we forget to close the file.
 
 # with statement
 # f = open("abc.txt", "w") , is same as, below declaration where 'as f' is an alias.
