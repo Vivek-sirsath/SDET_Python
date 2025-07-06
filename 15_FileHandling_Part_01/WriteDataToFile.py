d9b36f3f7e9c4e53699a7c95f1d74a27080fd799
vivek <vivek.sirsath1@gmail.com>
2025-07-06 23:45:36 +0530

    Extra code added

--- 15_FileHandling_Part_01/WriteDataToFile.py
+++ 15_FileHandling_Part_01/WriteDataToFile.py
@@ -20,3 +20,43 @@ f.close()
 
 f = open("pqr.txt", "a")
 f.write("I am Senior Test Engineer")
+
+f.close()
+
+print("=========== Checking what we've written in file ===========")
+
+f = open("pqr.txt", "r")
+lines = f.readlines()
+
+for line in lines:
+    print(line)
+
+f.close()
+
+"""
+CONSOLE OUTPUT:
+
+Hello everyone
+Good morning
+My name is Vivek
+I am Senior Test Engineer
+"""
+
+
+print("============== If the file is present in another directory ==============")
+# Here we need to specify that path along with file name.
+# Enclose the path in 'r-string', because copying the path will show error.
+
+# Open the file in append mode
+remoteFile = open(r"C:\Users\Admin\PycharmProjects\PythonProject\SDET_Python_Selenium\03_Operators\xyz.txt", "a")
+remoteFile.write("This is a remote file of example of File handling.")
+remoteFile.close()
+
+# Open the file in reading mode
+textFile = open(r"C:\Users\Admin\PycharmProjects\PythonProject\SDET_Python_Selenium\03_Operators\xyz.txt", "r")
+lines = textFile.readlines()
+
+for line in lines:
+    print(line)
+
+textFile.close()
