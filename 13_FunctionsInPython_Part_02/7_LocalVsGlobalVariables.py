06e3eb936a028831d2377478b06e95e00ecd2dbc
vivek <vivek.sirsath1@gmail.com>
2025-07-09 08:58:06 +0530

    Local variable vs Global variable comparison theoretical concept added.

--- 13_FunctionsInPython_Part_02/7_LocalVsGlobalVariables.py
+++ 13_FunctionsInPython_Part_02/7_LocalVsGlobalVariables.py
@@ -1,4 +1,19 @@
-# LOCAL VARIABLE vs GLOBAL VARIABLE
+"""
+
+LOCAL VARIABLE vs GLOBAL VARIABLE
+
+# TYPES OF VARIABLES:
+---------------------------------
+1) Global Variable :
+   - declared outside of function and accessible within all function in a same module.
+   - Global variable can be accessible within all the functions in the same package.
+   - Global variables are class/module level variables. But we can make any variable
+     as global variable inside a method by using 'global' keyword in front of it.
+
+2) Local Variable :
+   declared inside of function and accessible only within that funct ion.
+
+"""
 
 company_name = "Microsoft"  # Global Variable
 
@@ -14,7 +29,7 @@ def company_info():
 def company_address():
     print(f"Address: {company_name} , {location}")  # Here we've made 'location' as global variable.
 
-employee_details()  # Employee: John Legend, Company: Microsoft
+employee_details()  # Employee: John Legend, Location: Pune, Company: Microsoft
 company_info()  # Welcome to, Microsoft
 company_address()  # Address: Microsoft , Pune
 
