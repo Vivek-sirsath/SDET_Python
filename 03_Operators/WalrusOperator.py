2e1feba817eb2e40a373caf742d23cfb825f471b
vivek <vivek.sirsath1@gmail.com>
2025-07-21 08:36:31 +0530

    WalrusOperator.py added

---
+++ 03_Operators/WalrusOperator.py
@@ -0,0 +1,15 @@
+# WALRUS OPERATOR / NEW ASSIGNMENT EXPRESSION (:=)
+
+# 1) The conventional way -
+# Normal way of assigning value to a variable
+
+name = "Sam"
+print(name)
+
+
+# 2) Using Walrus Operator (:=) -
+# This operator combines declaration and implementation at the same time.
+# It is also called new assignment expression.
+
+print(name := "John")
+
