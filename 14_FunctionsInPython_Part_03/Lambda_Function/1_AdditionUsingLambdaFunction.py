ffd0da6a7ef8df3383a446b946c3a2571619db33
vivek <vivek.sirsath1@gmail.com>
2025-07-28 22:26:22 +0530

    Info about lambda function added

---
+++ 14_FunctionsInPython_Part_03/Lambda_Function/1_AdditionUsingLambdaFunction.py
@@ -0,0 +1,21 @@
+# Normal Function
+def add(a,b):
+    print(a+b)
+
+add(30,20)  # 50
+
+# Lambda Function
+s = lambda a,b:a+b
+
+print(s(15,10))  # 25
+
+
+"""
+NOTE: Since lambda functions are nameless anonymous functions, we store them in a variable 
+      that will act as a function name.
+      
+      Instead of 'def' we will use 'lambda' keyword.
+"""
+
+
+
