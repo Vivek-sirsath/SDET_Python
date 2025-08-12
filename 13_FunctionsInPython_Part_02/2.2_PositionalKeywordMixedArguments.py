508f6db4859712c9e84c37c477a120930a34e820
vivek <vivek.sirsath1@gmail.com>
2025-08-12 22:06:59 +0530

    Example of 'flipped positional arguments' added

--- 13_FunctionsInPython_Part_02/2.2_PositionalKeywordMixedArguments.py
+++ 13_FunctionsInPython_Part_02/2.2_PositionalKeywordMixedArguments.py
@@ -2,27 +2,10 @@
 # But, first we should specify Positional and then Keyword Arguments.
 
 def greet(name, msg):
-    print("Hello", name,msg)
+    print("Hello", name, msg)
 
-greet("Isha","Good Morning!")  # Hello Isha Good Morning!  # VALID
-greet("Isha", msg = "Good Morning!")  # Hello Isha Good Morning!  # VALID
+greet("Isha","Good morning!")  # Hello Isha Good Morning!   # VALID
+greet("Good afternoon", "Deepika")   # Hello Good afternoon Deepika   # VALID
+greet("Isha", msg = "Good evening!")  # Hello Isha Good Morning!   # VALID
 # greet(name= "Isha","Good Morning..!")   # SyntaxError: positional argument follows keyword argument   # INVALID
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
