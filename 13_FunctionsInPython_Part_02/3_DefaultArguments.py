b18431b5bc2c3fb727c107a20f5565f9a5e9baa4
vivek <vivek.sirsath1@gmail.com>
2025-08-13 08:57:33 +0530

    A function getResult() added in DefaultArguments.py

--- 13_FunctionsInPython_Part_02/3_DefaultArguments.py
+++ 13_FunctionsInPython_Part_02/3_DefaultArguments.py
@@ -4,6 +4,8 @@
    - If not provided values in function call, default values are considered.
    - After Default Arguments, we should not take Non Default Arguments in sequence. It will give syntax error.
    - Valid Order --> Positional > Keyword > Default
+
+   NOTE: Always follow the order P > K > D
 """
 
 def greet(name = "Deepika", msg = "how are you..?"):
@@ -14,6 +16,13 @@ greet(msg="good morning...!")   # Hello Deepika good morning...!
 greet(name="Monika")  # Hello Monika how are you..?
 greet(name="Emily",msg="I'm busy, will call you back...!")  # Hello Emily I'm busy, will call you back...!
 
+# After Default Arguments, we should not take Non Default Arguments in sequence. It will give syntax error.
+def getResult(name, marks, subject = "English"):
+    print(name, "got", marks, "marks in", subject,)
+
+getResult("Deepika", 95)   # Deepika got 95 marks in English   # VALID
+getResult(98,"Vivek")  # 98 got Vivek marks in English  # INVALID
+getResult(marks = 98,name = "Vivek")   # Vivek got 98 marks in English
 
 
 print("===========================================================================================================")
