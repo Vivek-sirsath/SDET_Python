e28b542f9b5b4a222f17d9f22fd093426fddf0d6
vivek <vivek.sirsath1@gmail.com>
2025-08-21 08:13:42 +0530

    Description added

--- 13_FunctionsInPython_Part_02/6.1_FirstOrderFunction.py
+++ 13_FunctionsInPython_Part_02/6.1_FirstOrderFunction.py
@@ -1,5 +1,13 @@
 # FIRST ORDER FUNCTION :
 
+"""
+   - A simple function is a First Order Function, without a function as an argument,
+     just containing simple data types.
+   - Do not take any other function as an argument.
+   - Do not return a function as a result.
+   - Only operates on basic data types. Does not take any other function as an argument.
+"""
+
 def div(i,j):
     print(i/j)
 
